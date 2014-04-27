b1d822cddf42624b8974ec60987047dbd83482e7
Tyler Garrett <jtgarrett123@gmail.com>
2014-04-26 20:19:00 -0700

    site has pagination

--- public/main.js
+++ public/main.js
@@ -5,4 +5,19 @@ celebrityApp.controller ("CelebrityHalflifeController", function($scope, $http) 
 	$http.get("/api/celebrities").success(function(data) {
 		$scope.celebs = data;
 	})
+
+	$scope.currentPage = 0;
+    $scope.pageSize = 10;
+    //$scope.data = [];
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.celebs.length/$scope.pageSize);
+    }
+});
+
+
+celebrityApp.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; //parse to int
+        return input.slice(start);
+    }
 });
