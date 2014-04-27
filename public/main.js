e9e969c2c9a4170b57753f12c0021aaf9a3ac8d8
Tyler Garrett <jtgarrett123@gmail.com>
2014-04-26 19:21:18 -0700

    angular app has access to api

--- public/main.js
+++ public/main.js
@@ -2,17 +2,7 @@ var celebrityApp = angular.module('celebrityApp', []);
 
 celebrityApp.controller ("CelebrityHalflifeController", function($scope, $http) {
 	$scope.test = "Testing";
-	$http.get("pathhere").success(function(data) {
+	$http.get("/api/celebrities").success(function(data) {
 		$scope.celebs = data;
 	})
 });
-
-//
-//
-// phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
-//   $http.get('phones/phones.json').success(function(data) {
-//     $scope.phones = data;
-//   });
-//
-//   $scope.orderProp = 'age';
-// });
