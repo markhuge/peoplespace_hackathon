392ecda12555842e71246119488574eff239a86e
Mark <mark@segfawlt.net>
2014-04-29 18:15:49 -0700

    making the linter happier

--- public/main.js
+++ public/main.js
@@ -4,14 +4,14 @@ celebrityApp.controller ("CelebrityHalflifeController", function($scope, $http) 
 	$scope.test = "Testing";
 	$http.get("/api/celebrities").success(function(data) {
 		$scope.celebs = data;
-	})
+	});
 
 	$scope.currentPage = 0;
     $scope.pageSize = 5;
     //$scope.data = [];
     $scope.numberOfPages=function(){
         return Math.ceil($scope.celebs.length/$scope.pageSize);
-    }
+    };
 });
 
 
@@ -19,10 +19,10 @@ celebrityApp.filter('startFrom', function() {
     return function(input, start) {
         start = +start; //parse to int
         return input.slice(start);
-    }
+    };
 });
 
 
-celebrityApp.filter('largeImage', function() { 
-    return function(image){ return image.replace("_normal","") };
+celebrityApp.filter('largeImage', function() {
+    return function(image){ return image.replace("_normal","")};
 });
